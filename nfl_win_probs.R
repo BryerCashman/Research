cb68c739524785df347e7347319b4b385db401db
BryerCashman <bryercashman@icloud.com>
2025-10-05 19:51:19 -0700

    Update nfl_win_probs.R

--- nfl_win_probs.R
+++ nfl_win_probs.R
@@ -1,7 +1,12 @@
 library(tidyverse)
 library(nflreadr)
+library(mgcv)
 
-load("~/Documents/GitHub/Research/proj_model.RDS")
+computer <- "W"
+
+path <- ifelse(computer == "W", "C:/Users/b.cashman/Documents/GitHub/Research/proj_model.RDS","/Users/bryer/Documents/GitHub/Research/proj_model.RDS")
+
+load(path)
 
 ewm_irregular_lagged <- function(x, days_gap, base) {
   stopifnot(length(x) == length(days_gap))
@@ -20,11 +25,12 @@ ewm_irregular_lagged <- function(x, days_gap, base) {
   out
 }
 
-base <- optimal_beta
+base <- optimal_beta <- 0.9974176
 
 data <- load_pbp(2021:2025) %>%
   filter((rush == 1 | pass == 1) ) %>%
   mutate(name = ifelse(name == "G.Minshew II","G.Minshew",name),
+         name = ifelse(name == "Aa.Rodgers", "A.Rodgers",name),
          game_date = as.Date(game_date))
 
 
@@ -117,7 +123,7 @@ qb_data2123 <- data %>%
   # snapshot what we knew at kickoff: take the first play's value for each game
   group_by(season, name, id, game_id, game_date) %>%
   summarize(qb_epa = first(ewm_qb_epa_play), 
-            dbs = first(dbs), 
+            dbs = first(dbs),
             .groups = "drop")
 
 qb_data2224 <- data %>%
@@ -231,12 +237,44 @@ defense_2325 <- data %>%
 
 defense <- rbind(defense_2123, defense_2224, defense_2325)  
 
+rm(qb_data2123, qb_data2224, qb_data2325, offense_2123, offense_2224, offense_2325, defense_2123, defense_2224, defense_2325)
 
 df_spreads <- schedule %>%
-  select(game_id,home_team, away_team, home_qb, home_qb_id, away_qb, away_qb_id) %>%
+  filter(result != 0) %>%
+  select(game_id,home_team, away_team, home_qb, home_qb_id, away_qb, away_qb_id, result) %>%
   left_join(offense_full, by = c("game_id", "home_team" = "posteam")) %>%
   rename(home_epa_pp = epa_pp) %>%
-  left_join(offense_full, by = c("game_id", "away_team" = "posteam")) %>%
+  left_join(offense_full, by = c("game_id","game_date", "away_team" = "posteam")) %>%
   rename(away_epa_pp = epa_pp) %>%
-  left_join(qb_data, by = c("game_id", "home_qb_id" = "id")) %>%
-  rename(home_qb_epa_per_play = qb_epa,)
+  left_join(qb_data, by = c("game_id","game_date", "home_qb_id" = "id")) %>%
+  rename(home_qb_epa_per_play = qb_epa, home_total_db = dbs ) %>%
+  left_join(qb_data, by = c("game_id","game_date", "away_qb_id" = "id")) %>%
+  rename(away_qb_epa_per_play = qb_epa, away_total_db = dbs ) %>%
+  left_join(defense, by = c("game_id","game_date", "home_team" = "defteam")) %>%
+  rename(home_epa_pp_allowed = "epa_pp_allowed") %>%
+  left_join(defense, by = c("game_id","game_date", "away_team" = "defteam")) %>%
+  rename(away_epa_pp_allowed = "epa_pp_allowed") %>%
+  mutate(weight = ifelse(season.x == 2021,1,2),
+         home_w = ifelse(result > 0, 1,0))
+
+df_spreads$x_point_diff <- predict(proj_model, df_spreads, type = "response")
+
+dt <- sample(nrow(df_spreads), .75 * nrow(df_spreads))
+train <- df_spreads[dt,]
+test <- df_spreads[-dt,]
+
+model_wp <- glm(home_w ~ x_point_diff, data = df_spreads, weights = weight, family = "binomial")
+
+summary(model_wp)
+
+test$home_wp <- predict(model_wp, test, type = "response")
+Metrics::logLoss(test$home_w[!is.na(test$home_wp)], test$home_wp[!is.na(test$home_wp)])
+
+grid <- expand.grid(x_point_diff = c(-15:15))
+grid$home_wp <- predict(model_wp, grid, type = "response")
+
+ggplot(grid, aes(x_point_diff, home_wp)) +
+  geom_line() +
+  theme_bw()
+
+model_home_wp <- model_wp
