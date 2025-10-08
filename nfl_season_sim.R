b3e25eda62405323f4d23f6102f469f30e6f110d
BryerCashman <bryercashman@icloud.com>
2025-10-08 12:04:35 -0700

    sim

---
+++ nfl_season_sim.R
@@ -0,0 +1,103 @@
+library(tidyverse)
+library(nflreadr)
+library(MASS)
+library(mgcv)
+
+
+def_epa_sd <- .0861
+sigma <- read.csv("C:/Users/b.cashman/Documents/csv/NFL/cov_matrix.csv")
+
+data <- load_pbp(2023:2025) %>%
+  filter((rush == 1 | pass == 1) ) %>%
+  mutate(name = ifelse(name == "G.Minshew II","G.Minshew",name))
+
+schedule <- load_schedules(2025) %>%
+  mutate(home_qb = str_c(str_sub(home_qb_name, 1, 1), ".", str_extract(home_qb_name, "[^ ]+$")),
+         away_qb = str_c(str_sub(away_qb_name, 1, 1), ".", str_extract(away_qb_name, "[^ ]+$")))
+
+
+computer <- "W"
+
+path <- ifelse(computer == "W", "C:/Users/b.cashman/Documents/GitHub/Research/proj_model.RDS","/Users/bryer/Documents/GitHub/Research/proj_model.RDS")
+load(path)
+load("C:/Users/b.cashman/Documents/R scripts/NFL/model_nfl_total.RDS")
+
+
+current_week <- max(schedule$week[!is.na(schedule$home_qb_name)])
+
+sunday <- schedule %>%
+  filter(week == current_week) %>%
+  group_by(date = gameday) %>%
+  dplyr::summarize(count = n()) %>%
+  ungroup() %>%
+  slice_max(order_by = count,n = 1) %>%
+  pull(date) %>%
+  as.Date()
+
+B <- optimal_beta <- 0.9974176
+
+master_qb_list <- unique(rbind(schedule$home_qb_id, schedule$away_qb_id))
+
+
+offense_data <- data %>%
+  filter(!is.na(posteam)) %>%
+  mutate(days_diff = as.numeric(difftime(sunday,game_date, "days"))) %>%
+  group_by(posteam) %>%
+  dplyr::summarize(epa_per_play = sum(epa * B^days_diff, na.rm = T)/sum(B^days_diff, na.rm = T),
+                   #pass_epa_per_play = mean((epa[pass == 1] * B^days_diff)/B^days_diff,na.rm = T),
+                   run_epa_per_play = sum((epa * B^days_diff)[rush==1], na.rm = T)/sum(B^days_diff[rush == 1], na.rm = T),
+                   #success_rate = mean((success * B^days_diff)/B^days_diff,na.rm = T),
+                   #plays = n()
+  ) %>%
+  ungroup() %>%
+  mutate(date = sunday)
+
+defense_data <- data %>%
+  filter(!is.na(defteam)) %>%
+  mutate(days_diff = as.numeric(difftime(sunday,game_date, "days"))) %>%
+  group_by(defteam) %>%
+  dplyr::summarize(epa_per_play_allowed =  sum(epa * B^days_diff, na.rm = T)/sum(B^days_diff, na.rm = T),
+                   #pass_epa_per_play_allowed = mean((epa[pass == 1] * B^days_diff)/B^days_diff,na.rm = T),
+                   run_epa_per_play_allowed = sum((epa * B^days_diff)[rush==1], na.rm = T)/sum(B^days_diff[rush == 1], na.rm = T),
+                   #success_rate_allowed = mean((success * B^days_diff)/B^days_diff,na.rm = T),
+                   #plays = n()
+  ) %>%
+  ungroup() %>%
+  mutate(date = sunday)
+
+qb_data <- data %>%
+  filter(!is.na(qb_epa),id %in% master_qb_list) %>%
+  mutate(days_diff = as.numeric(difftime(sunday,game_date, "days"))) %>%
+  group_by(id,name) %>%
+  mutate(qb_success = ifelse(qb_epa > 0,1,0)) %>%
+  dplyr::summarize(qb_epa_per_play = sum((qb_epa * B^days_diff), na.rm = T)/sum(B^days_diff, na.rm = T),
+                   #qb_total_epa_two_year = sum((qb_epa *B^days_diff)/B^days_diff ,na.rm = T),
+                   games_played = length(unique(game_id)),
+                   #qb_epa_per_game = qb_total_epa_two_year/games_played,
+                   #qb_success_rate = mean((qb_success *B^days_diff)/B^days_diff,na.rm = T),
+                   dropbacks_per_game = sum(qb_dropback,na.rm = T)/games_played
+  ) %>%
+  ungroup() %>%
+  mutate(total_dbs = dropbacks_per_game * games_played) %>%
+  filter(dropbacks_per_game > 3 )
+
+
+current_qbs <- rbind(schedule %>% dplyr::select(team = home_team,qb = home_qb,week) %>% filter(week %in% c(current_week,current_week + 1,current_week - 1)),
+                     schedule %>% dplyr::select(team = away_team,qb = away_qb,week) %>% filter(week %in% c(current_week,current_week + 1,current_week - 1))) %>%
+  na.omit() %>%
+  slice_max(order_by = week, n = 1, by = team) %>%
+  dplyr::select(team,qb) %>%
+  unique()
+
+current_ratings <- current_qbs %>%
+  inner_join(qb_data %>% dplyr::select(name, qb_epa_per_play, total_dbs), by = c("qb" = "name")) %>%
+  inner_join(offense_data %>% dplyr::select(posteam, epa_per_play), by = c("team"="posteam")) %>%
+  inner_join(defense_data %>% dplyr::select(defteam, epa_per_play_allowed), by = c("team" = "defteam"))
+
+
+new_cor <- mvrnorm(n = 32, mu = rep(0, 2), Sigma = sigma)
+
+new_ratings <- current_ratings
+new_ratings[, c("qb_epa_per_play", "epa_per_play")] <- current_ratings[, c("qb_epa_per_play", "epa_per_play")] + new_cor       
+
+new_ratings[,c("epa_per_play_allowed")] <- current_ratings[,c("epa_per_play_allowed")] + rnorm(32, sd = def_epa_sd)
--- sim_sds.R
+++ sim_sds.R
@@ -8,9 +8,10 @@ data <- load_pbp(2019:2025) %>%
          name = ifelse(name == "Aa.Rodgers", "A.Rodgers",name),
          game_date = as.Date(game_date))
 
-qb_yearly <- data %>%
+
+qb_deltas <- data %>%
   filter(!is.na(qb_epa), pass == 1 | rush == 1, season_type == "REG") %>%
-  group_by(name, id, season) %>%
+  group_by(name, id,posteam, season) %>%
   dplyr::summarize(mean_qb_epa = mean(qb_epa),
                    dbs = sum(qb_dropback),
                    total_epa = sum(qb_epa),
@@ -18,10 +19,60 @@ qb_yearly <- data %>%
   filter(dbs > 200) %>%
   ungroup() %>%
   arrange(name, id, season) %>%
-  group_by()
+  group_by(name, id) %>%
+  mutate(delta_qb_epa = mean_qb_epa - lag(mean_qb_epa)) %>%
+  ungroup()
+
+offense_deltas <- data %>%
+  filter(!is.na(epa), season_type == "REG", pass == 1 | rush == 1, season != 2025) %>%
+  group_by(posteam, season) %>% 
+  dplyr::summarize(epa_pp = mean(epa)) %>%
+  ungroup() %>%
+  arrange(posteam, season) %>%
+  group_by(posteam) %>%
+  mutate(delta_epa = epa_pp - lag(epa_pp)) %>%
+  ungroup()
+
+defense_deltas <- data %>%
+  filter(!is.na(epa), season_type == "REG", pass == 1 | rush == 1, season != 2025) %>%
+  group_by(defteam, season) %>% 
+  dplyr::summarize(epa_pp_allowed = mean(epa)) %>%
+  ungroup() %>%
+  arrange(defteam, season) %>%
+  group_by(defteam) %>%
+  mutate(delta_epa_allowed = epa_pp_allowed - lag(epa_pp_allowed)) %>%
+  ungroup()
+
+
+sd_shock_qb <- qb_deltas %>%
+  dplyr::summarize(sd_qb = sd(delta_qb_epa, na.rm = T))
+
+sd_shock_off <- offense_deltas %>%
+  dplyr::summarize(sd_off = sd(delta_epa, na.rm = T))
+
+sd_shock_def <- defense_deltas %>%
+  dplyr::summarize(sd_def = sd(delta_epa_allowed, na.rm = T))
 
 
 
+cov_d <- inner_join(qb_deltas, offense_deltas, by = c("posteam", "season")) %>% na.omit() %>% dplyr::select(delta_qb_epa, delta_epa)
 
 
+sigma <- cov(cov_d)
 
+
+test_features <- data.frame(qb_epa = c(.01, .22, -.03), team_epa = c(.01,-.02, .28))
+
+x_star <- test_features + mvrnorm(1, mu = c(0,0), Sigma = sigma)
+x_star
+
+test_features$adjusted <- test_features + mvrnorm(1, mu = c(0,0), Sigma = sigma)
+
+
+p <- 2
+e <- mvrnorm(n = 3, mu = rep(0,p), Sigma = sigma)
+colnames(e) <- paste0(cols, "_shock")
+xnew <- test_features + e
+
+
+write.csv(sigma, file = "C:/Users/b.cashman/Documents/csv/NFL/cov_matrix.csv", row.names = F)
